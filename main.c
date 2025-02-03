9226cb985657bcd2f92022a3d2767f26958f5fbe
Caleb Stein <caleb@calebstein.net>
2025-02-03 15:36:04 -0800

    add hashing function

--- main.c
+++ main.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 
-int main(void) {
-	printf("Hello, World!\n");
+#include <arm_acle.h>
+
+unsigned int
+get_hash(const char *data) {
+	char *d = (char *)data;
+	unsigned int crc = 0xffffffff;
+
+	while(*d) crc = __crc32cb(crc, *d++);
+
+	return crc ^ 0xffffffff;
+}
+
+int
+main(void) {
+	printf("%u\n", get_hash("Hello, world!"));
 	return 0;
 }
