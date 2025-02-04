9587a276492bdb314fea2c0ac000d994dd61881d
Caleb Stein <caleb@calebstein.net>
2025-02-03 16:17:03 -0800

    add GetValue function

--- main.c
+++ main.c
@@ -2,8 +2,13 @@
 
 #include <arm_acle.h>
 
-unsigned int
-get_hash(const char *data) {
+typedef struct {
+    void *d;
+    unsigned h;
+} HT_HashTable;
+
+unsigned
+HT_GetHash(const char *data) {
 	char *d = (char *)data;
 	unsigned int crc = 0xffffffff;
 
@@ -13,7 +18,31 @@ get_hash(const char *data) {
 }
 
 int
+HT_ProbeForBucket(const HT_HashTable *t, unsigned hash, unsigned i, size_t len) {
+	unsigned s = i ? i - 1 : len - 1;
+
+	while (t[i].h != hash) {
+		if (++i == len) i = 0;
+		if (i == s) return -1;
+	}
+
+	return (int)i;
+}
+
+void *
+HT_GetValue(const char *key, const HT_HashTable *t, size_t len) {
+	unsigned hash = HT_GetHash(key);
+	int i = (int)(hash % len);
+
+	if (t[i].h == hash) return t[i].d;
+
+	i = HT_ProbeForBucket(t, hash, i, len);
+
+	return i == -1 ? NULL : t[i].d;
+}
+
+int
 main(void) {
-	printf("%u\n", get_hash("Hello, world!"));
+
 	return 0;
 }
