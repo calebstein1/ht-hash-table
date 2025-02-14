1710fddc85c47574f2c72e83fa3d7e73e8b214ea
Caleb Stein <caleb@calebstein.net>
2025-02-14 12:41:23 -0800

    fix deletion gap bug

--- ht_hash.c
+++ ht_hash.c
@@ -4,9 +4,16 @@
 #define PRIME 0x01000193
 #define INIT_TBL_SIZE 8
 
+enum hash_status_e {
+	HT_FREE,
+	HT_USED,
+	HT_DELETED
+};
+
 struct hash_entry_t {
 	intptr_t d;
-	unsigned h, u;
+	unsigned h;
+	enum hash_status_e s;
 	char k[MAX_KEY_LEN];
 };
 
@@ -38,7 +45,7 @@ hash_table_t *HT_IncreaseSizeRehash(struct hash_table_t *tbl) {
 	if (!(ntbl = calloc(tbl->s << 1, sizeof(struct hash_entry_t)))) return NULL;
 	tbl->c = 0;
 	otbl = tbl->d, tbl->d = ntbl;
-	for (i = 0, tbl->s <<= 1; i < tbl->s >> 1; i++) if (otbl[i].u) HT_SetValue(otbl[i].k, otbl[i].d, tbl);
+	for (i = 0, tbl->s <<= 1; i < tbl->s >> 1; i++) if (otbl[i].s) HT_SetValue(otbl[i].k, otbl[i].d, tbl);
 	free(otbl);
 
 	return tbl;
@@ -62,7 +69,7 @@ HT_ProbeForBucket(const struct hash_table_t *t, unsigned hash, int i, int set) {
 	int len = t->s, s = i ? i - 1 : len;
 
 	while (t->d[i].h != hash) {
-		if (set && !t->d[i].u) break;
+		if (set && (!t->d[i].s || t->d[i].s == HT_DELETED)) break;
 		if (i == s) return -1;
 		if (++i == len) i = 0;
 	}
@@ -88,7 +95,7 @@ HT_SetValue(const char *key, intptr_t val, HT_HashTable t) {
 	i = (int)(hash & (t->s - 1));
 	if (t->d[i].h && (t->d[i].h != hash || strcmp(t->d[i].k, key) != 0)) i = HT_ProbeForBucket(t, hash, i, 1);
 	if (i == -1) return 0;
-	t->d[i].d = val, t->d[i].h = hash, t->d[i].u = 1, t->c++;
+	t->d[i].d = val, t->d[i].h = hash, t->d[i].s = HT_USED, t->c++;
 	strlcpy(t->d[i].k, key, MAX_KEY_LEN - 1);
 	return hash;
 }
@@ -100,7 +107,7 @@ HT_DeleteKey(const char *key, HT_HashTable t) {
 
 	if (t->d[i].h != hash || strcmp(t->d[i].k, key) != 0)
 		if ((i = HT_ProbeForBucket(t, hash, i, 0)) == -1) return;
-	memset(&t->d[i], 0, sizeof(struct hash_entry_t));
+	t->d[i].s = HT_DELETED;
 	t->c--;
 }
 
